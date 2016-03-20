864d9763bdce01b2f2ea5d4e1ddc3b98690b8362
Yuji Takaesu <taka.esu.user@gmail.com>
2016-03-21 00:00:40 +0900

    tweetbox on react

---
+++ tweetbox/README.md
@@ -0,0 +1,5 @@
+tweetbox sample
+============
+
+
+http://reactfordesigners.com/labs/reactjs-introduction-for-people-who-know-just-enough-jquery-to-get-by/
---
+++ tweetbox/index.jsx
@@ -0,0 +1,59 @@
+var TweetBox = React.createClass({
+  getInitialState: function() {
+    return {
+      text: "",
+      photoAdded: false
+    };
+  },
+
+  handleChange: function(event) {
+    this.setState({ text: event.target.value });
+  },
+
+  togglePhoto: function(event) {
+    this.setState({photoAdded: !this.state.photoAdded});
+  },
+
+  remainingCharacters: function() {
+    var photoStringSize = this.state.photoAdded ? 23 : 0;
+    return 140 - photoStringSize - this.state.text.length;
+  },
+
+  overflowAlert: function() {
+    if (this.remainingCharacters() < 0) {
+    baseSize = this.state.photoAdded ? 140-23 : 140;
+
+    var beforeOverflowText = this.state.text.substring(baseSize - 10, 140);
+    var overflowText = this.state.text.substring(baseSize);
+
+      return (
+        <div className="alert alert-warning">
+          <strong>Oops! Too Long:</strong>&nbsp;...{beforeOverflowText}<strong className="bg-danger">{overflowText}</strong>
+        </div>
+      );
+    } else {
+      return "";
+    }
+  },
+
+  render: function() {
+    return (
+      <div className="well clearfix">
+        { this.overflowAlert() }
+        <textarea className="form-control"
+                  onChange={this.handleChange}>
+        </textarea>
+        <br/>
+        <span>{ this.remainingCharacters() }</span>
+        <button className="btn btn-primary pull-right"
+                disabled={this.remainingCharacters() === 140}>Tweet</button>
+        <button className="btn btn-default pull-right" onClick={this.togglePhoto}>{this.state.photoAdded ? "✓ Photo Added" : "Add Photo"}</button>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <TweetBox />,
+  document.getElementById("container")
+);
