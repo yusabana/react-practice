70d85efcfdf08d9ebcbac031fe18db10f8797f90
Yuji Takaesu <taka.esu.user@gmail.com>
2018-03-18 20:01:25 +0900

    feat: enabled Hello component

---
+++ egghead-tutorial/beginner-guide/src/Hello.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const Hello = ({ name }) => {
+  return (
+    <Fragment>
+      <h1>Hello {name}!</h1>
+    </Fragment>
+  )
+}
+
+export default Hello
--- egghead-tutorial/beginner-guide/src/index.js
+++ egghead-tutorial/beginner-guide/src/index.js
@@ -1,19 +1,22 @@
-import React from "react"
-import { render } from "react-dom"
-// import PropTypes from "prop-types"
-import Timer from "./Timer"
-import Box from "./Box"
-import Event from "./Event"
+import React from 'react'
+import { render } from 'react-dom'
+// import PropTypes from 'prop-types'
+import Hello from './Hello'
+import Timer from './Timer'
+import Box from './Box'
+import Event from './Event'
 
 const Message = props => <div>{props.children}</div>
+
 const App = () => (
-  <div className="container">
+  <div className='container'>
+    <Hello name='Hoge' />
     <Message>Hello World</Message>
     <Timer />
-    <Box size="small" style={{ backgroundColor: "lightblue" }}>
+    <Box size='small' style={{ backgroundColor: 'lightblue' }}>
       Small Box
     </Box>
     <Event />
   </div>
 )
-render(<App />, document.getElementById("root"))
+render(<App />, document.getElementById('root'))
