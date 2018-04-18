689f40403af6f7790badef3feed8a34e6ed3e7a2
Yuji Takaesu <taka.esu.user@gmail.com>
2018-04-18 20:52:13 +0900

    sec4: react-router pages by SearchPage and AboutPage

--- udemy_react/src/components/App.jsx
+++ udemy_react/src/components/App.jsx
@@ -1,122 +1,25 @@
-import React, { Component } from 'react'
-import _ from 'lodash'
+import React from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import SearchPage from './SearchPage'
+import AboutPage from './AboutPage'
 
-import SearchForm from './SearchForm'
-import GeocodeResult from './GeocodeResult'
-import Map from './Map'
-import HotelsTable from './HotelsTable'
-import { geocode } from '../domain/Geocoder'
-import { searchHotelByLocation } from '../domain/HotelRepository'
-
-const sortedHotels = (hotels, sortKey) => _.sortBy(hotels, h => h[sortKey])
-
-class App extends Component {
-  constructor(props) {
-    console.log('constructor')
-    super(props)
-    this.state = {
-      address: '',
-      location: {
-        lat: 35.6585804,
-        lng: 139.7454329,
-      },
-      hotels: [],
-      sortKey: 'price',
-    }
-  }
-
-  componentWillMount() {
-    console.log('componentWillMount')
-  }
-
-  componentDidMount() {
-    console.log('componentDidMount')
-  }
-
-  componentWillReceiveProps() {
-    console.log('componentWillReceiveProps')
-  }
-
-  shouldComponentUpdate() {
-    console.log('shouldComponentUpdate')
-    return true // falseを返すと処理が止まってコンポーネントが再描画されない
-  }
-
-  componentWillUpdate() {
-    console.log('componentWillUpdate')
-  }
-
-  componentDidUpdate() {
-    console.log('componentDidUpdate')
-  }
-
-  componentWillUnmount() {
-    console.log('componentWillUnmount')
-  }
-
-  setErrorMessage(message) {
-    this.setState({
-      address: message,
-      location: {
-        lat: 0,
-        lng: 0,
-      },
-    })
-  }
-
-  handlePlaceSubmit(place) {
-    geocode(place)
-      .then(({ status, address, location }) => {
-        switch (status) {
-          case 'OK': {
-            this.setState({ address, location })
-            return searchHotelByLocation(location)
-          }
-          case 'ZERO_RESULTS': {
-            this.setErrorMessage('結果が見つかりません')
-            break
-          }
-          default: {
-            this.setErrorMessage('エラーが発生しました')
-          }
-        }
-        return []
-      })
-      .then(hotels => {
-        // ↑で返された hotels を受け取ってstateに設定して処理する
-        this.setState({ hotels: sortedHotels(hotels, this.state.sortKey) })
-      })
-      .catch(error => {
-        console.log(`ERROR :: ${error}`)
-        this.setErrorMessage('通信に失敗しました')
-      })
-  }
-
-  handleSortKeyChange(sortKey) {
-    this.setState({ sortKey, hotels: sortedHotels(this.state.hotels, sortKey) })
-  }
-
-  render() {
-    console.log('render')
-    return (
-      <div className="app">
-        <h1 className="app-title">ホテル検索</h1>
-        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
-        <div className="result-area">
-          <Map location={this.state.location} />
-          <div className="result-right">
-            <GeocodeResult address={this.state.address} location={this.state.location} />
-            <h2>ホテル検索結果</h2>
-            <HotelsTable
-              hotels={this.state.hotels}
-              sortKey={this.state.sortKey}
-              onSort={sortKey => this.handleSortKeyChange(sortKey)}
-            />
-          </div>
-        </div>
-      </div>
-    )
-  }
-}
+const App = () => (
+  <Router>
+    <div className="app">
+      <ul className="left-navi">
+        <li>
+          <Link to="/">ホテル検索</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/" component={SearchPage} />
+        <Route exact path="/about" component={AboutPage} />
+      </Switch>
+    </div>
+  </Router>
+)
 
 export default App
